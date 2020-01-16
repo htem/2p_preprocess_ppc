b1cbd7f88087c9eff2b2e66d8ef9c73fd729fca5
Aaron Kuan <aaron.t.kuan@gmail.com>
2020-01-16 17:11:09 -0500

    minimal check of trial alignment against session 33. conv still needs updating

--- new_LD187_preprocessing.m
+++ new_LD187_preprocessing.m
@@ -13,6 +13,8 @@ for s = 1:size(sessionList,1)
     sessionList_all{s,1} = sessionList(s,1).name;
 end
 session = sessionList_all{36,1};
+trialAlignedData = struct;
+
 %% line up 
 lineUpSession(session) % for now run 141213
 % Exports VirminCombined to ppc/2P_data/code_workspace/LD187/virmen/FILE.mat
@@ -32,26 +34,27 @@ vData = load(linedUpPath);
 neucoeff = 0.7;
 s2p.Fsub = s2p.F - neucoeff * s2p.Fneu;
 
+% for now use suite2p's deconvolution
+spks = s2p.spks;
+% dF = zscore(s2p.Fsub,0,2);
 %[c, s, options] = deconvolveCa(s2p.Fsub(1,:));
 %[c,b,c1,g,sn,sp] = constrained_foopsi(s2p.Fsub(1:2,:));
 
-% for now skip deconvolution
-dF = zscore(s2p.Fsub,0,2);
-
 %% Plot example traces
 figure; hold on;
-frames = 1:length(dF);
+frames = 1:length(spks);
 dt = 1/5.3; % 5.3 Hz sampling rate
 time = frames * dt;
 for i = 1:30%size(mySignal,1)
-    plot(time,dF(i,:)+3*i);
+    plot(time,spks(i,:)+3*i);
 end
 xlabel('time (sec)');
 ylabel('dF/F');
 
+
 %% Parse virmen trials
 
-trialAlignedData = parseVirmenTrials(vData.VirmenCombined, dF);
+trialAlignedData = parseVirmenTrials(vData.VirmenCombined, spks);
 
 % break into individual trial types
 trialAlignedData.bR_trials_Ca = trialAlignedData.CaData(:,trialAlignedData.trialType==2,:);
@@ -62,7 +65,7 @@ trialAlignedData.wL_trials_virmen = trialAlignedData.virmenData(:,trialAlignedDa
 trialAlignedData.new_trials_virmen = trialAlignedData.virmenData(:,trialAlignedData.trialType==4,:);
 
 %% Sanity check with trialwise behavioral data
-beh_idx = 2;
+beh_idx = 4;
 figure; hold on;
 for i = 1:size(trialAlignedData.bR_trials_virmen,2)
     plot(squeeze(trialAlignedData.bR_trials_virmen(beh_idx,i,:)),'b-')
@@ -72,12 +75,164 @@ for i = 1:size(trialAlignedData.wL_trials_virmen,2)
 end    
 for i = 1:size(trialAlignedData.new_trials_virmen,2)
     plot(squeeze(trialAlignedData.new_trials_virmen(beh_idx,i,:)),'k-')
-end    
+end
+
+
+%% Calculate trial average activities
+
+trialAlignedData.bR_Ca_trialMean = squeeze(mean(trialAlignedData.bR_trials_Ca,2));
+trialAlignedData.wL_Ca_trialMean = squeeze(mean(trialAlignedData.wL_trials_Ca,2));
+trialAlignedData.new_Ca_trialMean = squeeze(mean(trialAlignedData.new_trials_Ca,2));
+
+%% Calculate R/L selectivities
+% exclude ITI-before (which is unrelated to this trial), 13:76
+thisTrial = 13:76;
+r = mean(trialAlignedData.bR_Ca_trialMean(:,thisTrial),2);
+l = mean(trialAlignedData.wL_Ca_trialMean(:,thisTrial),2);
+trialAlignedData.cellSelectIdx = (r - l)./(r + l);
+figure; histogram(trialAlignedData.cellSelectIdx)
+% definition fo cellSelectIdx from Harvey 2012 
+
+% sanity-check activity for very selective cells
+figure; hold on; 
+subplot(1,2,1);
+plot(thisTrial,trialAlignedData.bR_Ca_trialMean(trialAlignedData.cellSelectIdx>0.45,13:76)')
+ylim([0,25]);
+subplot(1,2,2); 
+plot(thisTrial,trialAlignedData.wL_Ca_trialMean(trialAlignedData.cellSelectIdx>0.45,13:76)')
+ylim([0,25]);
+
+%% Calculate pairwise correlation matrices
+
+trialAlignedData.corr_all = corrcoef(spks');
+trialAlignedData.corr_bR_trials = corrcoef(trialAlignedData.bR_Ca_trialMean');
+trialAlignedData.corr_wL_trials = corrcoef(trialAlignedData.wL_Ca_trialMean');
+
+% sanity check by comparing
+figure;
+plot(trialAlignedData.corr_all(:),trialAlignedData.corr_bR_trials(:),'.');
+xlabel('total correlation'); ylabel('bR trial correlation');
+
+figure;
+plot(trialAlignedData.corr_bR_trials(:),trialAlignedData.corr_wL_trials(:),'.');
+xlabel('bR trials correlations'); ylabel('wL trials correlation');
+
+%% Calculate time center-of-mass 
+
+rmean = trialAlignedData.bR_Ca_trialMean(:,thisTrial);
+lmean = trialAlignedData.wL_Ca_trialMean(:,thisTrial);
+trialAlignedData.bR_tCOM = (rmean*thisTrial')./sum(rmean,2);
+trialAlignedData.wL_tCOM = (lmean*thisTrial')./sum(lmean,2);
+
+% sanity check by plotting high COM 
+figure;
+plot(trialAlignedData.bR_Ca_trialMean(trialAlignedData.bR_tCOM>48,:)');
+
+% sanity check by RL correlation
+figure; plot(trialAlignedData.bR_tCOM,trialAlignedData.wL_tCOM,'o');
+xlabel('bR_tCOM');ylabel('wL_tCOM');
+
+%% Calculate time maximum
+for i = 1:size(rmean,1)
+    bR_tMax(i) = find(rmean(i,:) == max(rmean(i,:)),1,'last');
+    wL_tMax(i) = find(lmean(i,:) == max(lmean(i,:)),1,'last');
+end
+%wL_tMax = find(lmean == max(lmean,2));
+%[m, wL_tMax] = max(lmean,2);
+
+% sanity check by plotting high COM 
+figure;
+plot(trialAlignedData.bR_Ca_trialMean(bR_tMax>55,:)');
+
+% sanity check by plotting
+figure; plot(bR_tMax,wL_tMax,'.');
+xlabel('bR_tCOM');ylabel('wL_tCOM');
+
+%% Plot example activity as test
+
+s2p_cid = 1351;
+matlab_cid = s2p_cid+1;
+figure;
+
+subplot(1,2,1); hold on;
+plot(cueBlockEarly,trialAlignedData.bR_Ca_trialMean(matlab_cid,cueBlockEarly),'color',cueEarlyColor);
+plot(cueBlockLate,trialAlignedData.bR_Ca_trialMean(matlab_cid,cueBlockLate),'color',cueLateColor);
+plot(delayBlockEarly,trialAlignedData.bR_Ca_trialMean(matlab_cid,delayBlockEarly),'color',delayEarlyColor);
+plot(delayTurnBlock,trialAlignedData.bR_Ca_trialMean(matlab_cid,delayTurnBlock),'color',delayTurnColor);
+plot(turnITIblock,trialAlignedData.bR_Ca_trialMean(matlab_cid,turnITIblock),'color',turnITIcolor);
+title('right');
+
+subplot(1,2,2); hold on;
+plot(cueBlockEarly,trialAlignedData.wL_Ca_trialMean(matlab_cid,cueBlockEarly),'color',cueEarlyColor);
+plot(cueBlockLate,trialAlignedData.wL_Ca_trialMean(matlab_cid,cueBlockLate),'color',cueLateColor);
+plot(delayBlockEarly,trialAlignedData.wL_Ca_trialMean(matlab_cid,delayBlockEarly),'color',delayEarlyColor);
+plot(delayTurnBlock,trialAlignedData.wL_Ca_trialMean(matlab_cid,delayTurnBlock),'color',delayTurnColor);
+plot(turnITIblock,trialAlignedData.wL_Ca_trialMean(matlab_cid,turnITIblock),'color',turnITIcolor);
+title('left');
+%%
+figure; hold on;
+subplot(1,2,1);
+plot(trialAlignedData.bR_Ca_trialMean(matlab_cid,thisTrial));
+title('right');
+subplot(1,2,2);
+plot(trialAlignedData.wL_Ca_trialMean(matlab_cid,thisTrial));
+title('left');
+
+%%
+plot(cueBlockEarly+toffset,spData_all(cIdx,cueBlockEarly,sIdx,k)+j*yoffset,'color',cueEarlyColor);
+plot(cueBlockLate+2*toffset,spData_all(cIdx,cueBlockLate,sIdx,k)+j*yoffset,'color',cueLateColor);
+plot(delayBlockEarly+2*toffset,spData_all(cIdx,delayBlockEarly,sIdx,k)+j*yoffset,'color',delayEarlyColor);
+plot(delayTurnBlock+3*toffset,spData_all(cIdx,delayTurnBlock,sIdx,k)+j*yoffset,'color',delayTurnColor);
+plot(turnITIblock+3*toffset,spData_all(cIdx,turnITIblock,sIdx,k)+j*yoffset,'color',turnITIcolor);
+            
+for k = 1:2
+    subplot(1,2,k); hold on; axis off; % title(lr(k));
+    %text(-12,i*yoffset, num2str(cIdx));
+    line([runOnset+toffset runOnset+toffset],[0 yoffset]+yoffset*(earliestSession-1),'color','k');
+    text(runOnset+toffset, yoffset * (earliestSession-1), 'Run','Rotation',90,'HorizontalAlignment','right','color','g');
+    line([cueOff+2*toffset cueOff+2*toffset],[0 yoffset]+yoffset*(earliestSession-1),'color','k');
+    text(cueOff+2*toffset, yoffset * (earliestSession-1), 'Cue Off','Rotation',90,'HorizontalAlignment','right','color','r');
+    line([trialEnd+3*toffset trialEnd+3*toffset],[0 yoffset]+yoffset*(earliestSession-1),'color','k');
+    text(trialEnd+3*toffset, yoffset * (earliestSession-1), 'End','Rotation',90,'HorizontalAlignment','right','color','b');
+    for j = 1:length(mySessions)
+        sIdx = mySessions(j);
+        if ~isnan(spData_tAvg(cIdx,mySessions(j),k))
+            if k == 2
+                text(-10,j*yoffset, ['Session ' num2str(mySessions(j))],'HorizontalAlignment','center');
+            end
+            %timingColor = [cueCoef(cIdx,j,k) delayCoef(cIdx,j,k) turnCoef(cIdx,j,k)].^cpower;
+            timingColor = 'b';
+            %plot(ITIbeforeBlock,spData_all(cIdx,ITIbeforeBlock,sIdx,k)+j*yoffset,'color','k');
+
+            % Display selectivity indices (colored text)
+            s = cellSelectIdx(cIdx,j);
+            lr_color = lr_cmap(round(s*100)+101,:);
+            subplot(1,2,2); text(t(end)+10*toffset,j*yoffset,num2str(s,2),...
+                'color',lr_color,'BackgroundColor',[.5 .5 .5]);
+            subplot(1,2,k); hold on; axis off;
+        end
+    end
+    axis([0 max(t)+3*toffset (earliestSession-2)*yoffset (latestSession+3)*yoffset]);
+end
+
+
+%% Quantify signal-to-noise
+
+% From raw (non-trial aligned) traces.
+snr_ratio_raw = prctile(s2p.Fsub,90,2)./abs(prctile(s2p.Fsub,10,2));
+figure; histogram(snr_ratio_raw);
+
+%% Sanity check by plotting high and low snr 
+figure;
+plot(s2p.Fsub(snr_ratio_raw>1.3,:)');
+%%
+figure; hold on;
+plot(trialAlignedData.bR_Ca_trialMean(1:30,:)')
 %% Plot trial avg dF
 figure; hold on;
 frames = 1:size(trialAvg_dF,2);
 time = frames;% * acq.metaDataSI.SI4.fastZPeriod;
-for i = 1:size(dF,1)
+for i = 1:size(spks,1)
     plot(time,trialAvg_dF(i,:));
 end
 xlabel('time (frames)');
@@ -141,7 +296,7 @@ subplot(2,2,4);imagesc(zTrialMean_L(cellOrderL,:));title('L sorted by L');
 figure; hold on;
 frames = 1:size(trialAvg_dF,2);
 time = frames(12:64);% * acq.metaDataSI.SI4.fastZPeriod;
-for i = 1:size(dF,1)
+for i = 1:size(spks,1)
     plot(time,zTrialMean(cellOrder(i),12:64)+0.3*i);
 end
 xlabel('time (frames)');
