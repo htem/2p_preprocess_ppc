47e00eb7c3faeed6937d484806ce7f8f69fef4dd
Aaron Kuan <aaron.t.kuan@gmail.com>
2020-05-07 15:44:42 -0400

    new snr metrics and corrrelations

--- new_LD187_preprocessing.m
+++ new_LD187_preprocessing.m
@@ -16,7 +16,7 @@ end
 %% Select Session and lineup
 
 %%%%%%%%%
-session = sessionList_all{31,1};
+session = sessionList_all{38,1};
 disp(session)
 %%%%%%%%%
 
@@ -37,17 +37,10 @@ disp(['Suite2p video data has ' num2str(size(s2p.F,2)) ' frames']);
 linedUpPath = fullfile(masterPath,'code_workspace',mouse,'virmen',[session '.mat']);
 vData = load(linedUpPath);
 disp(['Virmen Data has ' num2str(size(vData.VirmenCombined,2)) ' frames']);
-%% Deconvolution
 neucoeff = 0.7;
 s2p.Fsub = s2p.F - neucoeff * s2p.Fneu;
 
-% for now use suite2p's deconvolution
-% spks = s2p.spks;
-% dF = zscore(s2p.Fsub,0,2);
-%
-
-% Deconvolution with constrained_foopsi
-%[c, s, options] = deconvolveCa(s2p.Fsub(1,:),'method','constrained');
+%%
 tic
 [c,b,c1,g,sn,sp] = run_constrained_foopsi(double(s2p.Fsub));
 toc
@@ -57,26 +50,34 @@ if ~exist(output_dir_deconv,'dir')
     mkdir(output_dir_deconv)
 end
 save(fullfile(output_dir_deconv,session),'c','b','c1','g','sn','sp');
-%% compare suite2p and constrained_foopsi deconv
-cell_idx = find(s2p.iscell(:,1)==1);
 
-figure;
-num_ex = 8;
-start = 100;
-num_pts = 400;
-tsec = (1:num_pts)/5.6;
-for i = start:start+num_ex-1
-    %disp(cell_idx(i))
-    subplot(num_ex,3,3*(i-start)+1);
-    plot(tsec,s2p.Fsub(cell_idx(i),1:num_pts));
-    title('raw');
-    subplot(num_ex,3,3*(i-start) + 2);
-    plot(tsec,spks(cell_idx(i),1:num_pts));
-    title('suite2p');
-    subplot(num_ex,3,3*(i-start)+3);    
-    plot(tsec,sp(cell_idx(i),1:num_pts));
-    title('const foopsi');
+%% Load deconv data
+
+output_dir_deconv = fullfile(masterPath, 'code_workspace',mouse,'deconvData');   
+load(fullfile(output_dir_deconv,session),'c','b','c1','g','sn','sp');
+
+%% Calculate SNR metrics
+snr = nan([],length(sn));
+for i = 1:length(sn)
+    snr(i) = std(c(i,:))^2/sn(i)^2;
 end
+
+%% Plot SNR 
+figure; plot(snr); xlabel('mask ID'); ylabel('SNR');
+figure; histogram(snr, 100); xlabel('SNR'); ylabel('# of masks');  xlim([0,150]);
+%%
+cid =674;
+dF = s2p.Fsub;
+sorted_dF = sort(dF(cid,:));
+baseline_adj = mode(round(100*sorted_dF(.05*size(sorted_dF,2):...
+        .95*size(sorted_dF,2)))/100);
+dF_zeroed = dF(cid,:) - baseline_adj;
+
+figure; hold on; 
+plot(dF_zeroed);
+%plot(c(cid,:));
+title(['SNR: '  num2str(snr(cid))])
+
 %% Parse virmen trials
 % Hack for 141210 %% trialAlignedData = parseVirmenTrials(vData.VirmenCombined(:,6001:16000), sp);
 trialAlignedData = parseVirmenTrials(vData.VirmenCombined, sp);
@@ -84,6 +85,7 @@ output_dir = fullfile(masterPath, 'code_workspace',mouse,'syncedData');
 if ~exist(output_dir,'dir')
     mkdir(output_dir)
 end
+trialAlignedData.SNR_raw = snr;
 save(fullfile(output_dir,session),'trialAlignedData');
 
 %% Plot example traces
@@ -138,3 +140,24 @@ plot(delayTurnBlock,trialAlignedData.wL_trials.Ca_trialMean(matlab_cid,delayTurn
 plot(turnITIblock,trialAlignedData.wL_trials.Ca_trialMean(matlab_cid,turnITIblock),'color',turnITIcolor);
 title('left');
 linkaxes([ax1 ax2],'xy')
+
+%% compare suite2p and constrained_foopsi deconv
+cell_idx = find(s2p.iscell(:,1)==1);
+
+figure;
+num_ex = 3;
+start = 100;
+num_pts = 400;
+tsec = (1:num_pts)/5.6;
+for i = start:start+num_ex-1
+    disp(cell_idx(i))
+    subplot(num_ex,3,3*(i-start)+1);
+    plot(tsec,s2p.Fsub(cell_idx(i),1:num_pts));
+    title('raw');
+    subplot(num_ex,3,3*(i-start) + 2);
+    plot(tsec,s2p.spks(cell_idx(i),1:num_pts));
+    title('suite2p');
+    subplot(num_ex,3,3*(i-start)+3);    
+    plot(tsec,sp(cell_idx(i),1:num_pts));
+    title('const foopsi');
+end
--- parseVirmenTrials.m
+++ parseVirmenTrials.m
@@ -119,12 +119,31 @@ trialTypes = {'newL_trials','bR_trials','wL_trials','newR_trials'};
 for i = 1:length(trialTypes)
     % Ca activity and virmen activity for each trial type individually
     trialAlignedData.(trialTypes{i}) = struct;
-    trialAlignedData.(trialTypes{i}).Ca = trialAlignedData.CaData(:,trialAlignedData.trialType==i,:);
-    trialAlignedData.(trialTypes{i}).virmen = trialAlignedData.virmenData(:,trialAlignedData.trialType==i,:);
-
+    Ca = trialAlignedData.CaData(:,trialAlignedData.trialType==i,14:76); % DON"T INCLUDE ITI BEFORE
+    trialAlignedData.(trialTypes{i}).Ca = Ca;
+    concatSz = [size(Ca,1), size(Ca,2)*size(Ca,3)];
+    trialAlignedData.(trialTypes{i}).Ca_concat = reshape(permute(Ca,[1,3,2]), concatSz);
+    virmen = trialAlignedData.virmenData(:,trialAlignedData.trialType==i,14:76);
+    trialAlignedData.(trialTypes{i}).virmen = virmen;
+    numTrials = size(virmen,2);
+    trialAlignedData.(trialTypes{i}).numTrials = numTrials;
     % Calculate trial average activities
-    trialAlignedData.(trialTypes{i}).Ca_trialMean = squeeze(mean(trialAlignedData.(trialTypes{i}).Ca,2));
+    trialMean = squeeze(mean(trialAlignedData.(trialTypes{i}).Ca,2));
+    trialAlignedData.(trialTypes{i}).Ca_trialMean = trialMean;
+    trialAlignedData.(trialTypes{i}).Ca_trialMean_concat = repmat(trialMean,1,size(Ca,2));
+    trialResidual = trialAlignedData.(trialTypes{i}).Ca - permute(repmat(trialMean,[1,1,numTrials]),[1,3,2]);
+    trialAlignedData.(trialTypes{i}).Ca_residual = trialResidual;
+    trialAlignedData.(trialTypes{i}).Ca_residual_concat = reshape(permute(trialResidual,[1,3,2]), [concatSz]);
+    % Calculated trial SNR
+    trial_snr = std(trialAlignedData.(trialTypes{i}).Ca_trialMean_concat,0,2).^2./std(trialAlignedData.(trialTypes{i}).Ca_residual_concat,0,2).^2;
+    trialAlignedData.(trialTypes{i}).trial_snr = trial_snr; 
 end
+
+trialAlignedData.Ca_concat = cat(2,trialAlignedData.bR_trials.Ca_concat,trialAlignedData.wL_trials.Ca_concat);
+trialAlignedData.Ca_trialMean_concat = ...
+    cat(2,trialAlignedData.bR_trials.Ca_trialMean_concat,trialAlignedData.wL_trials.Ca_trialMean_concat);
+trialAlignedData.Ca_residual_concat = ...
+    cat(2,trialAlignedData.bR_trials.Ca_residual_concat,trialAlignedData.wL_trials.Ca_residual_concat);
 
 %% Sanity check with trialwise behavioral data
 beh_idx = 2;
@@ -139,7 +158,8 @@ for j = 1:length(trialTypes)
     end
 end
 %% Calculate R/L selectivity indices
-thisTrial = 13:76;
+%thisTrial = 13:76;
+thisTrial = 1:63;
 % exclude ITI-before (which is unrelated to this trial), 13:76
 r = nanmean(trialAlignedData.bR_trials.Ca_trialMean(:,thisTrial),2);
 l = nanmean(trialAlignedData.wL_trials.Ca_trialMean(:,thisTrial),2);
@@ -147,6 +167,7 @@ trialAlignedData.RL_selectIdx = (r - l)./(r + l);
 figure; histogram(trialAlignedData.RL_selectIdx)
 % definition fo cellSelectIdx from Harvey 2012 
 
+%{
 % sanity-check activity for very selective cells
 figure; hold on; 
 subplot(1,2,1);
@@ -155,18 +176,23 @@ ylim([0,25]);
 subplot(1,2,2); 
 plot(thisTrial,trialAlignedData.wL_trials.Ca_trialMean(trialAlignedData.RL_selectIdx>0.4,thisTrial)')
 ylim([0,25]);
-
+%}
 
 
 %% Calculate pairwise correlation matrices
 
 trialAlignedData.corr_all = corrcoef(syncCaData');
+trialAlignedData.corr_Ca_concat = corrcoef(trialAlignedData.Ca_concat');
+trialAlignedData.corr_Ca_trialMean = corrcoef(trialAlignedData.Ca_trialMean_concat');
+trialAlignedData.corr_Ca_residual = corrcoef(trialAlignedData.Ca_residual_concat');
 for i = 1:length(trialTypes)
     if size(trialAlignedData.(trialTypes{i}).virmen,2) > 0
         disp(trialTypes{i})
         trialAlignedData.(trialTypes{i}).corrcoef = corrcoef(trialAlignedData.(trialTypes{i}).Ca_trialMean','rows','complete');
     end
 end
+
+%{
 % sanity check by comparing
 figure;
 plot(trialAlignedData.corr_all(:),trialAlignedData.bR_trials.corrcoef(:),'.');
@@ -176,9 +202,20 @@ figure;
 plot(trialAlignedData.bR_trials.corrcoef(:),trialAlignedData.wL_trials.corrcoef(:),'.');
 xlabel('bR trials correlations'); ylabel('wL trials correlation');
 
+figure; plot(trialAlignedData.corr_all(:),trialAlignedData.corr_Ca_concat(:),'.');
+xlabel('trialAlignedData.corr_all'); ylabel('trialAlignedData.corr_Ca_concat');
+
+figure; plot(trialAlignedData.corr_Ca_concat(:),trialAlignedData.corr_Ca_trialMean(:),'.');
+xlabel('trialAlignedData.corr_Ca_concat'); ylabel('trialAlignedData.corr_Ca_trialMean');
+
+figure; plot(trialAlignedData.corr_Ca_concat(:),trialAlignedData.corr_Ca_residual(:),'.');
+xlabel('trialAlignedData.corr_Ca_concat'); ylabel('trialAlignedData.corr_Ca_residual');
+
+figure; plot(trialAlignedData.corr_Ca_trialMean(:),trialAlignedData.corr_Ca_residual(:),'.');
+xlabel('trialAlignedData.corr_Ca_trialMean_concat'); ylabel('trialAlignedData.corr_Ca_residual');
+%}
+
 %% Calculate timing metrics
-
-
 for i = 1:length(trialTypes)
     if size(trialAlignedData.(trialTypes{i}).virmen,2) > 0
         Ca = trialAlignedData.(trialTypes{i}).Ca_trialMean;
@@ -192,49 +229,18 @@ for i = 1:length(trialTypes)
         trialAlignedData.(trialTypes{i}).tMax = tMax;
     end
 end
-% sanity check by plotting high COM 
-figure;
-plot(trialAlignedData.bR_trials.Ca_trialMean(trialAlignedData.bR_trials.tCOM>48,:)');
 
+% sanity check by plotting high COM 
+%{
+figure;
+plot(trialAlignedData.bR_trials.Ca_trialMean(trialAlignedData.bR_trials.tCOM>48-13,:)');
+title('plot high (late) COM trials');
 % sanity check by plotting high tmax
 figure;
-plot(trialAlignedData.bR_trials.Ca_trialMean(trialAlignedData.bR_trials.tMax>70,:)');
-
-%% Calculate activity timing indices
-% ATK sanity check doesn't seem right, hold off for now
-%{
-% Define time blocks
-cueBlockEarly = 14:26; % 14 is running onset + 12 frames after
-cueBlockLate = 27:38; % 12 frames before cue offset (frame 39)
-delayBlockEarly = 39:51; % 39 is cue offset + 12 frames after
-delayTurnBlock = 52:64; % 12 frames before end of trial (turn a certain amt)
-turnITIblock = 65:76; % Trial end (reward given) an 12 frames after (dark, ITI)
-
-for i = 1:length(trialTypes)
-    if size(trialAlignedData.(trialTypes{i}).virmen,2) > 0
-        disp(['type ' trialTypes{i}])
-        meanCueEarly = squeeze(mean(trialAlignedData.(trialTypes{i}).Ca_trialMean(:,cueBlockEarly),2));
-        meanCueLate = squeeze(mean(trialAlignedData.(trialTypes{i}).Ca_trialMean(:,cueBlockLate),2)); 
-        meanDelayEarly = squeeze(mean(trialAlignedData.(trialTypes{i}).Ca_trialMean(:,delayBlockEarly),2));
-        meanDelayTurn = squeeze(mean(trialAlignedData.(trialTypes{i}).Ca_trialMean(:,delayTurnBlock),2));
-        meanTurnITI = squeeze(mean(trialAlignedData.(trialTypes{i}).Ca_trialMean(:,turnITIblock),2));
-
-        normSpRate = meanCueEarly + meanCueLate + meanDelayEarly + meanDelayTurn + meanTurnITI;
-        
-        trialAlignedData.(trialTypes{i}).t_selectIdx = [meanCueEarly meanCueLate meanDelayEarly meanDelayTurn meanTurnITI]./normSpRate;
-    end
-end
-
-% sanity check by plotting very selective traces
-figure;
-hold on;
-colors = jet(5);
-for tIdx = 5:5
-    disp(sum(trialAlignedData.wL_trials.t_selectIdx(:,tIdx)>.5))
-    %plot(trialAlignedData.wL_trials.Ca(trialAlignedData.wL_trials.t_selectIdx(:,tIdx)>.6,thisTrial)','color',colors(tIdx,:));
-    %plot(thisTrial,trialAlignedData.wL_trials.Ca(trialAlignedData.wL_trials.t_selectIdx(:,tIdx)>.5,thisTrial));
-    imagesc(trialAlignedData.wL_trials.Ca(trialAlignedData.wL_trials.t_selectIdx(:,tIdx)>.5,thisTrial));
-end
+plot(trialAlignedData.bR_trials.Ca_trialMean(trialAlignedData.bR_trials.tMax>70-13,:)');
+title('plot high (late) tmax trials');
 %}
+
+
 
 
